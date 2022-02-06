c3e7709daa02a51f6562a042f922cd15821dc5e5
Mahmoud <mahmoud.hany.c@stc.com.kw>
2022-02-06 09:12:31 +0200

    webpack setup done

---
+++ src/contentScript/contentScript.ts
@@ -0,0 +1,3 @@
+console.log('contentScript');
+
+// chrome.runtime.sendMessage()
--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,14 @@
 const path = require('path')
-const CopyPlugin = require('webpack-copy-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 const HtmlPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
-  devtool: 'cheap-module-source-map',
   entry: {
     popup: path.resolve('src/popup/popup.tsx'),
-    options: path.resolve('src/options/options.tsx')
+    options: path.resolve('src/options/options.tsx'),
+    background: path.resolve('src/background/background.ts'),
+    contentScript: path.resolve('src/contentScript/contentScript.ts')
   },
   module: {
     rules: [
@@ -27,12 +28,12 @@ module.exports = {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin({
+      cleansStaleWebpackAssets: false
+    }),
     new CopyPlugin({
       patterns: [
-        {
-          from: path.resolve("src/static/"),
-          to: path.resolve("dist/")
-        }
+        { from: "src/static" }
       ],
     }),
     ...getHtmlPlugins([
